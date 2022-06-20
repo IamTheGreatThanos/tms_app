298d4d57c722c7d4cae52805ae7fef778d734379
Aboneyx <abylay.asylbek02@gmail.com>
2022-06-20 15:35:22 +0600

    Updated Yandex Map Keys

--- android/app/src/main/kotlin/com/thousand/tmcsatti/MainActivity.kt
+++ android/app/src/main/kotlin/com/thousand/tmcsatti/MainActivity.kt
@@ -6,7 +6,8 @@ import io.flutter.plugins.GeneratedPluginRegistrant;
 import com.yandex.mapkit.MapKitFactory;
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        MapKitFactory.setApiKey("b51c2cab-1212-4f56-a22d-77d0a2632811") // Your generated API key
+        // MapKitFactory.setApiKey("b51c2cab-1212-4f56-a22d-77d0a2632811") // басындагы турган ключ
+        MapKitFactory.setApiKey("2a3a1619-cd5e-48dd-8564-83a064c7dac2")
         super.configureFlutterEngine(flutterEngine)
     }
 }
